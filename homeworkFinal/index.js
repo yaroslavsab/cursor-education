c206ede9a90f259f35013c50f4c4dc1f78b4637d
Yaroslav Sabadakh <saab_86@ukr.net>
2023-08-11 16:43:46 +0300

    re

--- homeworkFinal/index.js
+++ homeworkFinal/index.js
@@ -1,19 +1,3 @@
-let valueDiaplays = document.querySelectorAll(".total__digit");
-let interval = 8000;
-
-valueDiaplays.forEach((valueDispay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDispay.getAttribute("data-val"));
-    console.log(endValue);
-    let duration = setInterval(interval / endValue);
-    let counter = setInterval(function () {
-        startValue += 1;
-        valueDispay.textContent = startValue;
-        if (startValue === endValue) {
-            clearInterval(counter);
-        }
-    }, duration);
-});
 
 $(document).ready(function () {
     // Add smooth scrolling to all links
