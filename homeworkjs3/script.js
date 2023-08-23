fb765322434e54e10233d01d356e7de418013bdc
Yaroslav Sabadakh <saab_86@ukr.net>
2023-08-23 10:31:06 +0300

    4 tasks

--- homeworkjs3/script.js
+++ homeworkjs3/script.js
@@ -1,28 +1,35 @@
 function getMaxDigit(number) {
-    // Перетворюємо число на рядок, щоб працювати з окремими цифрами
     const numberStr = number.toString();
-
-    // Ініціалізуємо змінну для збереження найбільшої цифри
     let maxDigit = 0;
-
-    // Проходимо по кожному символу у рядку
     for (let i = 0; i < numberStr.length; i++) {
         const digit = parseInt(numberStr[i]);
-
-        // Порівнюємо поточну цифру з найбільшою знайденою
         if (digit > maxDigit) {
             maxDigit = digit;
         }
     }
-
-    // Повертаємо найбільшу цифру
     return maxDigit;
 }
 
 console.log("Найбільша цифра:", getMaxDigit(749283));
 
 
-
 const getSumTax = suma => suma - (suma / 100) * 19.5;
 
-console.log(getSumTax(1000))
+console.log("Відсоток", getSumTax(1000));
+
+function random(n, m) {
+    return (Math.random() * (m - n) + n).toFixed(0);
+}
+console.log("Випадкове число ві 1 до 10 - ", random(1, 10));
+
+function pow(n, step) {
+    let count = 1;
+    if (step > 0) {
+        for (let i = 0; i < step; i++) {
+            count *= n;
+        }
+    }
+    return count;
+}
+
+console.log("Ступінь числа -", pow(6, 2));
