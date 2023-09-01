2d5d65afd0b8d07e38a4afdfa68715aa363f316c
Yaroslav Sabadakh <saab_86@ukr.net>
2023-09-01 16:22:22 +0300

    5task

--- homeworkjs5/script.js
+++ homeworkjs5/script.js
@@ -16,8 +16,8 @@ function getRandomArray(length, min, max) {
     return array;
 
 }
-
 console.log("getRandomArray - ", getRandomArray(15, 1, 100));
+
 /*
 3. Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне
 всіх переданих в неї аргументів. НЕ ЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
@@ -25,12 +25,72 @@ console.log("getRandomArray - ", getRandomArray(15, 1, 100));
 
 function getAverage(...numbers) {
     let sum = 0;
-    let average = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        sum = numbers[i] + sum;
-    }
-
+    numbers.map((number) => {
+        if (Number.isInteger(number)) {
+            sum += number;
+        }
+        return sum;
+    });
     return sum / numbers.length;
+
 }
 
 console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+/*
+5. Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа
+передані як аргументи функції.
+Приклад: filterEvenNumbers(1, 2, 3, 4, 5, 6) -> [1, 3, 5]*/
+
+function filterEvenNumbers(...numbers) {
+
+    return numbers.filter((number) => {
+        return number % 2 !== 0;
+    })
+}
+
+console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6, 8));
+
+/*
+6. Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість
+чисел більших
+Приклад: countPositiveNumbers(1, -2, 3, -4, -5, 6) -> 3
+*/
+
+function countPositiveNumbers(...numbers) {
+    let count = 0;
+    numbers.map((number) => {
+
+        if (number > 0) {
+            count++;
+        }
+        return count;
+    });
+    return count;
+}
+
+console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6, 5, -2));
+
+/*
+7. Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи
+в масиві та залишить тільки ті, які діляться на ціло на 5
+Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55]
+*/
+
+const getDividedByFive = (...numbers) => numbers.filter((number) => number % 5 === 0);
+
+
+console.log(getDividedByFive(6, 25, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
+/*
+8. Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2)
+замінить погані слова на зірочки (*). При вирішенні цього завдання необхідно
+розбити масив на слова за допомогою функції .split(" "), після чого масив
+необхідно буде склеїти .join(" ") Погані слова: shit та fuck. Передбачте
+можливість розширювати cписок цих слів у майбутньому.
+Приклад: replaceBadWords("Are you fucking kidding?") -> "Are you ****ing
+kidding?"
+Приклад: replaceBadWords("Holy shit!") -> "Holy ***!"
+Приклад: replaceBadWords("It's bullshit!") -> "It's bull****!"
+*/
+
