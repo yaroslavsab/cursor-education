9bd669e5c341948b7d60769a7c368a4644fb8854
Yaroslav Sabadakh <saab_86@ukr.net>
2023-09-22 14:38:35 +0300

    done

---
+++ homeworkjs9/script.js
@@ -0,0 +1,38 @@
+// Функція для генерації одного квадрата з випадковим кольором
+function generateSquare() {
+    const container = document.getElementById('container');
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+    container.style.width = '300px';
+}
+
+// Функція для генерації 25 квадратів
+function generateBlocks() {
+    for (let i = 0; i < 25; i++) {
+        generateSquare();
+    }
+}
+
+// Функція для зміни кольору квадрату на випадковий
+function changeColor(square) {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16); // Генерація випадкового кольору
+    square.style.backgroundColor = randomColor;
+}
+
+// Функція для зміни кольору всіх квадратів на випадковий
+function changeColors() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        changeColor(square);
+    });
+}
+
+// Функція для генерації квадратів та зміни їх кольору кожну секунду
+function generateBlocksInterval() {
+    generateBlocks();
+    setInterval(changeColors, 1000);
+}
+
+// Викликаємо функцію для початку генерації квадратів та зміни їх кольору кожну секунду
+generateBlocksInterval();
