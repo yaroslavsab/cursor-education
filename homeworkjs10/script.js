531d81edf3c2394756c07fa774f8052ee96a3a40
Yaroslav Sabadakh <saab_86@ukr.net>
2023-09-25 17:20:22 +0300

    add letters

--- homeworkjs10/script.js
+++ homeworkjs10/script.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let cont = document.getElementById('container');
 
 
+    window.addEventListener('keydown', (e) => {
+        cont.innerHTML += e.key;
+    });
 
 
 })
