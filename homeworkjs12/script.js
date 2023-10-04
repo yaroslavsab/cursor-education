126628d9df972dfb25b43d9e9522eb8aed31b38b
Yaroslav Sabadakh <saab_86@ukr.net>
2023-10-04 17:12:44 +0300

    add styles

--- homeworkjs12/script.js
+++ homeworkjs12/script.js
@@ -1,15 +1,19 @@
 function renderCharacters({ name, birth_year, gender }) {
-    const div = document.createElement('div');
-    const h1 = document.createElement('h2');
 
-    h1.textContent = name;
-
-    div.append(h1);
-    document.body.append(div);
-    console.log(name, birth_year, gender);
+    const userTemplate = `
+    <div class="character">
+    <h2 class="character_title">${name}</h2>
+    <p class="year">Рік народження: ${birth_year}</p>
+    <span>стать: ${gender}</span>
+    </div>
+    `;
+    document.body.innerHTML += userTemplate;
 }
 
-
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    getSwapi()
+})
 
 function fetchCharacters(character) {
     fetch(character)
@@ -23,7 +27,6 @@ function getSwapi() {
     fetch("https://swapi.dev/api/films/2")
         .then((res) => res.json())
         .then((films) => {
-            // console.log(films.characters)
             films.characters.forEach(character => {
                 fetchCharacters(character)
             });
@@ -32,4 +35,3 @@ function getSwapi() {
 }
 
 
-getSwapi()
